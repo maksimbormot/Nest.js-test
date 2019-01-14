6f5f8ae7095d7d94eedcc8707811cabf6fd856fd
maksimbormot <maksim.bormot11@gmail.com>
2019-01-14 15:27:00 +0200

    Connect payments microservice to app controller

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices'
 import { AppModule } from './app.module';
 import { OrdersModule } from './orders/orders.module'
+import { PaymentsModule } from './payments/payments.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,5 +15,13 @@ async function bootstrap() {
     }
   });
   orders.listen(() => console.log('Orders Microservice is listening'));
+
+  const payments = await NestFactory.createMicroservice(PaymentsModule, {
+    transport: Transport.TCP,
+    options: {
+      port: 3002,
+    }
+  });
+  payments.listen(() => console.log('Payments Microservice is listening'));
 }
 bootstrap();
